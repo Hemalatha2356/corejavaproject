640fcbf115f0e5ffc4c9e39189287bc22a75d01c
hemalatha-allatipalli <hemalathaallatipalli@gmail.com>
2026-09-03 15:39:16 +0530

    Inheritance program

--- src/com/oops1/inheritancetypes/TestDemo3.java
+++ src/com/oops1/inheritancetypes/TestDemo3.java
@@ -1,9 +1,46 @@
 package com.oops1.inheritancetypes;
 
-public class TestDemo3 {
 
+class Course{
+	void duration() {
+		System.out.println("the Course duration!!");
+	}
+}
+
+class javaCourse extends Course{
+	@Override
+	void duration() {
+		System.out.println("javacourse duration is 3months!!");
+	}
+}
+
+class pythonCourse extends Course{
+	
+	void duration() {
+		System.out.println("pythoncourse duration is 2months!!");
+	}
+}
+
+class SQLCourse extends Course{
+	void duration() {
+		System.out.println("SQLCourse duration is 1month!!");
+	}
+}
+
+
+public class TestDemo3 {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Course c = new Course();
+		Course c1 = new javaCourse();
+		Course c2 = new pythonCourse();
+		Course c3 = new SQLCourse();
+		c.duration();
+		c1.duration();
+		c2.duration();
+		c3.duration();
+		
+		
 
 	}
 
